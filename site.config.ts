dd0971cf0868ad950880834778237f7b5618a64f
Chris Celaya <im-mr-chris@protonmail.ch>
2023-03-03 18:57:32 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Notion Website Test',
-  domain: 'notionsite',
+  name: 'Programming Fundamentals II',
+  domain: '',
   author: 'Christopher Celaya',
 
   // open graph metadata (optional)
